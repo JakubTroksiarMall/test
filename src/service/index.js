9c6fa767f07c6c14cb5530e7df728b87bbd6660a
Jakub Troksiar <jakub.troksiar@ecommerceholding.cz>
2017-04-10 16:40:39 +0200

    second

--- src/service/index.js
+++ src/service/index.js
@@ -1,18 +1,1 @@
-import Service from 'vue-service'
 
-const apiUrl = 'http://some.stuff/api/';
-
-export default someService extends Service {
-
-  construct(){
-    super(apiUrl)
-  }
-
-  getAll(){
-    return this.get('someEndPoint');
-  }
-
-  getOne(id) {
-    return this.get('someEndPoint/'+ id);
-  }
-}
